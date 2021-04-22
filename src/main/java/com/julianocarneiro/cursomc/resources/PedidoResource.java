26292545cd8947d1cfb6a613a06e922867715bad
Juliano Carneiro <julianoomc@gmail.com>
2021-04-22 20:04:38 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/julianocarneiro/cursomc/resources/PedidoResource.java
@@ -0,0 +1,31 @@
+package com.julianocarneiro.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.julianocarneiro.cursomc.domain.Pedido;
+import com.julianocarneiro.cursomc.services.PedidoService;
+
+
+//Criando classe que será um controlador REST e responderá pelo endpoint "/categorias"
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired //Controlador REST acessa camada de Serviço
+	private PedidoService service;
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Pedido obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+	
+}
---
+++ src/main/java/com/julianocarneiro/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.julianocarneiro.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.julianocarneiro.cursomc.domain.Pedido;
+import com.julianocarneiro.cursomc.repositories.PedidoRepository;
+import com.julianocarneiro.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired //instancia a dependencia declarada abaixo
+	private PedidoRepository repo; //Serviço acessa camada de acesso a dados (Repository)
+	
+	//operação capaz de buscar categoria por código (recebe id Integer como parâmetro)
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+
+}
