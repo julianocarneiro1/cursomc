681d4f83f53834249c42615cb294aa6303bf18d2
Juliano Carneiro <julianoomc@gmail.com>
2021-04-24 02:11:45 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/julianocarneiro/cursomc/services/CategoriaService.java
+++ src/main/java/com/julianocarneiro/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.julianocarneiro.cursomc.domain.Categoria;
+import com.julianocarneiro.cursomc.dto.CategoriaDTO;
 import com.julianocarneiro.cursomc.repositories.CategoriaRepository;
 import com.julianocarneiro.cursomc.services.exceptions.DataIntegrityException;
 import com.julianocarneiro.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,11 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	// método auxiliar que instancia uma categoria a partir de um DTO
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
