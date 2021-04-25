a125601af3940ff926602efa29d6e3f4ce506b47
Juliano Carneiro <julianoomc@gmail.com>
2021-04-25 19:05:50 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/julianocarneiro/cursomc/services/CategoriaService.java
+++ src/main/java/com/julianocarneiro/cursomc/services/CategoriaService.java
@@ -36,8 +36,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId()); // reaproveitando método para ver se id existe
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -62,6 +63,10 @@ public class CategoriaService {
 	// método auxiliar que instancia uma categoria a partir de um DTO
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 	
 }
