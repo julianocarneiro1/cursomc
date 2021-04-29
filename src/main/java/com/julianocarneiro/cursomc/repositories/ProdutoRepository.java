40f332355cfe48b59e0e6f1353dfd0b3aa974c30
Juliano Carneiro <julianoomc@gmail.com>
2021-04-29 19:36:28 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/julianocarneiro/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/julianocarneiro/cursomc/repositories/ProdutoRepository.java
@@ -1,12 +1,22 @@
 package com.julianocarneiro.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.julianocarneiro.cursomc.domain.Categoria;
 import com.julianocarneiro.cursomc.domain.Produto;
 
 @Repository
-// Classe responsável por acessar o BD e fazer as consultas da tabela Categoria
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-	
+
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
