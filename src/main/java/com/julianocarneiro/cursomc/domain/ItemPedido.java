1b28eb415134a04178153c29145b0e551876e641
Juliano Carneiro <julianoomc@gmail.com>
2021-05-04 11:45:25 -0300

    Subtotal e total do pedido

--- src/main/java/com/julianocarneiro/cursomc/domain/ItemPedido.java
+++ src/main/java/com/julianocarneiro/cursomc/domain/ItemPedido.java
@@ -19,6 +19,10 @@ public class ItemPedido implements Serializable {
 	private Integer quantidade;
 	private Double preco;
 	
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
 	public ItemPedido() {
 	}
 
